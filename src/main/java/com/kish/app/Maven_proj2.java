3d7af3e2cc46b2b4ee5cb45a61bcdffaf4d36881
Gitea <gitea@fake.local>
2021-02-24 15:31:11 +0530

    loo

---
+++ src/main/java/com/kish/app/Maven_proj2.java
@@ -0,0 +1,10 @@
+package com.kish.app;
+
+public class Maven_proj2 {
+	
+	public String Sample()
+	{
+		return "maven project";
+	}
+
+}
---
+++ src/test/java/com/kish/app/testmaven.java
@@ -0,0 +1,14 @@
+package com.kish.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testmaven {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
